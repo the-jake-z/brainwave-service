b191d24e171178a4f1a4b4d3f66e654b1b9a0d3c
Jake Zarobsky <jzarob@gmail.com>
2018-04-14 12:15:55 -0500

    Added flask server code

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 
 socketio = SocketIO(app)
+
+
+@socketio.on('connect')
+def client_connected():
+    print("Client connected")
+
+
+@socketio.on('disconnect')
+def client_disconnected():
+    print("Client disconnected")
+
+
+def send_data(data):
+    send(data)
 
 if __name__ == '__main__':
     socketio.run(app)
